e0a675f48764457bba45fc1dc7c9e5c17f3e3405
vishpraveen <vishpraveen007@gmail.com>
2023-09-18 16:13:30 +0530

    Abstract factory design pattern test code modification

--- src/test/kotlin/creational/abstractfactory/CompanyTest.kt
+++ src/test/kotlin/creational/abstractfactory/CompanyTest.kt
@@ -1,29 +1,46 @@
 package creational.abstractfactory
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
 
 class CompanyTest {
     @Test
     fun `Test AsusGpu Creation`() {
         val asus: Company = AsusGpuManufacturer()
-        asus.manufactureGPU()
+        assertTrue(
+            actual = asus.manufactureGPU() is AsusGpu,
+            message = String.format(MESSAGE, AsusGpu::class.simpleName)
+        )
     }
 
     @Test
     fun `Test MsiGpu Creation`() {
         val msi: Company = MsiGpuManufacturer()
-        msi.manufactureGPU()
+        assertTrue(
+            actual = msi.manufactureGPU() is MsiGpu,
+            message = String.format(MESSAGE, MsiGpu::class.simpleName)
+        )
     }
 
     @Test
     fun `Test AsusMonitor Creation`() {
         val asus: Company = AsusGpuManufacturer()
-        asus.manufactureMonitor()
+        assertTrue(
+            actual = asus.manufactureMonitor() is AsusMonitor,
+            message = String.format(MESSAGE, AsusMonitor::class.simpleName)
+        )
     }
 
     @Test
     fun `Test MsiMonitor Creation`() {
         val msi: Company = MsiGpuManufacturer()
-        msi.manufactureMonitor()
+        assertTrue(
+            actual = msi.manufactureMonitor() is MsiMonitor,
+            message = String.format(MESSAGE, MsiMonitor::class.simpleName)
+        )
+    }
+
+    private companion object {
+        const val MESSAGE = "object is instance of %s"
     }
 }
